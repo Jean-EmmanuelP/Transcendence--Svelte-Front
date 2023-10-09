7d53786efd496bc9f2eb9e210c218278f50254c1
Perramant <perramant.work@gmail.com>
2023-10-09 14:23:31 +0200

    doing now with urql because houdini i didnt succeed to setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,33 @@
-<script lang="ts">
+<script>
+  import { Client, cacheExchange, fetchExchange } from '@urql/core';
+  import { query, setContext } from '@urql/svelte';
+  import { gql } from 'graphql-tag';
+
+  // Initialisation du client
+  const client = new Client({
+    url: 'http://42pong.com:3000/graphql',
+    exchanges: [cacheExchange, fetchExchange],
+  });
+
+  setContext({ client }); // Passer le client au contexte
+
+  // Définir et exécuter la requête
+  const QUERY = gql`
+    query {
+      testingConnexion
+    }
+  `;
+
+  const test = query(QUERY);
+
 </script>
 
 <h1 class="text-3xl font-bold underline bg-red-500 text-center p-2 w-full h-full">Hello world!</h1>
-  <!-- <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style> -->
+
+{#if $test.fetching}
+  <p>Loading ...</p>
+{:else if $test.error}
+  <p>Oh no... {$test.error.message}</p>
+{:else}
+  <p>{$test.data.testingConnexion}</p>
+{/if}
--- src/urqlClient.ts
+++ src/urqlClient.ts
@@ -1,8 +1,8 @@
-import { Client, cacheExchange, fetchExchange } from '@urql/svelte'
+import { Client, cacheExchange, fetchExchange, setContextClient } from '@urql/svelte'
 
 const client = new Client({
     url: "http://42pong.com:3000/graphql",
     exchanges: [cacheExchange, fetchExchange],
 });
 
-export default client;
+setContextClient(client);
