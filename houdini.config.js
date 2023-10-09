6d5b89a95eb0c21b52189afe9ab2513ac10c8085
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-09 22:54:40 +0200

    for merge

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "http://42pong.com:3000/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  <!-- <style lang="postcss">
+<script lang="ts">
+	import type { PageData } from './$houdini';
+	export let data: PageData;
+	$: ({ testingConnexion } = data);
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+{$testingConnexion?.data}
+<!-- <style lang="postcss">
     :global(html) {
       background-color: theme(colors.gray.100);
     }
