844f6a61cab796e659906b72f4eaedd481bc2843
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-09 23:36:58 +0200

    added the login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -9,43 +9,13 @@
 			alt="Your Company"
 		/>
 		<h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-			Sign up 
+			Sign in
 		</h2>
 	</div>
 
 	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
 		<div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
 			<form class="space-y-6" action="#" method="POST">
-                <div>
-					<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-						>First Name</label
-					>
-					<div class="mt-2">
-						<input
-							id="email"
-							name="email"
-							type="email"
-							autocomplete="email"
-							required
-							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-						/>
-					</div>
-				</div>
-                <div>
-					<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-						>Last Name</label
-					>
-					<div class="mt-2">
-						<input
-							id="email"
-							name="email"
-							type="email"
-							autocomplete="email"
-							required
-							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-						/>
-					</div>
-				</div>
 				<div>
 					<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
 						>Email address</label
@@ -102,7 +72,7 @@
 					<button
 						type="submit"
 						class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-						>Sign up</button
+						>Sign in</button
 					>
 				</div>
 			</form>
@@ -172,9 +142,9 @@
 		</div>
 
 		<p class="mt-10 text-center text-sm text-gray-500">
-			Have an account?
-			<a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-				>Sign in</a
+			Not a member?
+			<a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+				>Register an account</a
 			>
 		</p>
 	</div>
