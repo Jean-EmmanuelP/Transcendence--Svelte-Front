88c3a4a984b2819ca1ef958c16151a45f2c9a8fe
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-10 19:10:45 +0200

    play button with more padding

--- src/components/play.svelte
+++ src/components/play.svelte
@@ -1,53 +1,73 @@
 <script>
     // Votre logique de script ici, si nécessaire
-  </script>
-  
-  <style>
-    .pong-field {
-      position: relative;
-    }
-  
-    .paddle {
-      position: absolute;
-      height: 80px;
-      width: 15px;
-      background-color: white;
-    }
-  
-    .left-paddle {
-      left: 20px;
-    }
-  
-    .right-paddle {
-      right: 20px;
-    }
-  
-    .center-line {
-      position: absolute;
-      width: 4px;
-      height: 100%;
-      background: repeating-linear-gradient(
-        to bottom,
-        white,
-        white 10px,
-        transparent 10px,
-        transparent 20px
-      );
-      left: 50%;
-      transform: translateX(-50%);
-    }
-  </style>
-  
-  <div class="h-full w-full flex items-center justify-center bg-black pong-field">
-    <div class="paddle left-paddle top-1/2 transform -translate-y-1/2"></div>
-    <div class="center-line"></div>
-    <div class="paddle right-paddle top-1/2 transform -translate-y-1/2"></div>
-  
-    <button
-      type="button"
-      class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-    >
-      Play Pong
-    </button>
-  </div>
-  
+</script>
+
+<style>
+  .pong-field {
+    position: relative;
+    /* Ajustements pour garantir que la "balle" (le bouton) soit visible */
+    min-height: 200px; /* Ajoutez une hauteur minimale */
+  }
+
+  .paddle {
+    position: absolute;
+    height: 80px;
+    width: 15px;
+    background-color: white;
+  }
+
+  .left-paddle {
+    left: 20px;
+  }
+
+  .right-paddle {
+    right: 20px;
+  }
+
+  .center-line {
+    position: absolute;
+    width: 4px;
+    height: 100%;
+    background: repeating-linear-gradient(
+      to bottom,
+      white,
+      white 10px,
+      transparent 10px,
+      transparent 20px
+    );
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .pong-ball {
+    /* Styles spécifiques pour le bouton "balle" */
+    width: 40px; /* Largeur du bouton */
+    height: 40px; /* Hauteur du bouton */
+    border-radius: 50%; /* Rend le bouton rond */
+    background-color: #4F46E5; /* Couleur de fond Indigo-600 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white; /* Texte blanc */
+    font-weight: 600; /* Font semi-bold */
+    cursor: pointer; /* Curseur de pointeur pour indiquer la cliquabilité */
+    z-index: 2;
+}
+
+  .pong-ball:hover {
+    background-color: #4338CA; /* Couleur Indigo-500 pour l'état de survol */
+  }
+</style>
+
+<div class="h-full w-full flex items-center justify-center bg-black pong-field rounded-md shadow-xl">
+  <div class="paddle left-paddle top-1/2 transform -translate-y-1/2"></div>
+  <div class="center-line"></div>
+  <div class="paddle right-paddle top-1/2 transform -translate-y-1/2"></div>
+
+  <button
+    type="button"
+    class="pong-ball p-[35px]"
+  >
+    Play
+  </button>
+</div>
