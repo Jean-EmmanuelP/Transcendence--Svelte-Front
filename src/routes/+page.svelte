f05885739b898ecc2286e9ca592da1e8ff9420d9
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-10 12:27:33 +0200

    added the navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
 	import Sidebar from "../components/sidebar.svelte";
+	import Navbar from "../components/navbar.svelte";
 </script>
 <div class="h-screen w-screen flex border border-red-500 overflow-hidden">
 
 	<div class="w-[25%] h-screen">
 		<Sidebar />
+	</div>
+	<div class="w-full">
+		<Navbar />
 	</div>
 	
 </div>
