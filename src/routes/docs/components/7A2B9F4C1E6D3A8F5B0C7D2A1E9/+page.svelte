14bae08dcdadbfe9f6fe8389c9df53cd7c4b40a1
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-10 14:47:02 +0200

    doing the documentation

--- src/routes/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9/+page.svelte
+++ src/routes/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Here are all our components</h1>
+<script>
+	const component = `/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9`;
+</script>
+
+<h1 class="text-center pt-2 font-bold">Here are all our components</h1>
+<div class="shadow-md rounded-md p-10">
+	<li class="flex flex-row gap-2">
+		<p>This is to create a group :</p>
+		<a href="{component}/create-group-form" class="underline"
+			>components/action_components/create-group-form</a
+		>
+	</li>
+	<li class="flex flex-row gap-2">
+		<p>this is when the user will click ctrl-K</p>
+		<a href="" />
+	</li>
+	<li class="flex flex-row gap-2">
+		<p />
+		<a href="" />
+	</li>
+	<li class="flex flex-row gap-2">
+		<p />
+		<a href="" />
+	</li>
+	<li class="flex flex-row gap-2">
+		<p />
+		<a href="" />
+	</li>
+	<li class="flex flex-row gap-2">
+		<p />
+		<a href="" />
+	</li>
+</div>
