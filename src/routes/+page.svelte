9381debd04326a1e24ed52361f8f32db26fb0585
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-10 18:29:44 +0200

    created the gif component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Sidebar from '$components/sidebar.svelte';
 	import Navbar from '$components/navbar.svelte';
 	import StoryPong from '$components/story_pong.svelte';
+	import Gif from '../components/gif.svelte';
 </script>
 
 <div class="h-screen w-screen flex overflow-hidden">
@@ -10,14 +11,8 @@
 	</div>
 	<div class="w-full h-screen flex flex-col">
 		<Navbar />
-		<div class="flex m-10 rounded-md h-1/2 w-1/2">
-			<iframe
-				src="https://giphy.com/embed/xUOxfhGCOiD9JS3ANG"
-				width="100%"
-				height="100%"
-				class="pointer-events-none"
-				allowFullScreen
-			/>
+		<div class="flex justify-center items-center h-full w-full">
+			<Gif />
 		</div>
 	</div>
 </div>
