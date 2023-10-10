bf0417c3dc43127f6e22711d4c077b5a0cd3e2ec
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-10 16:46:46 +0200

    doing the space for the game/ the story and the third space i dont know what it could be]

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,16 @@
 	<div class="w-[25%] h-screen">
 		<Sidebar />
 	</div>
-	<div class="w-full">
+	<div class="w-full h-screen flex flex-col">
 		<Navbar />
+		<div class="flex bg-red-500 m-5 h-full">
+			<div class="h-full w-[55%] bg-green-500">
+				<div class="h-1/3 w-full bg-yellow-500">
+
+				</div>
+			</div>
+			<div class="h-full w-1/2"></div>
+		</div>
 	</div>
 	
 </div>
