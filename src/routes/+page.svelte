3f846f6f2e75e0cfb0a0233596018c9a3c5050d0
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-10 18:20:32 +0200

    added a frame for the pong

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,14 @@
 	</div>
 	<div class="w-full h-screen flex flex-col">
 		<Navbar />
-		<div class="flex bg-red-500 m-10 rounded-md h-full ring-1 ring-gray-400/5">
-			
+		<div class="flex m-10 rounded-md h-1/2 w-1/2">
+			<iframe
+				src="https://giphy.com/embed/xUOxfhGCOiD9JS3ANG"
+				width="100%"
+				height="100%"
+				class="pointer-events-none"
+				allowFullScreen
+			/>
 		</div>
 	</div>
 </div>
