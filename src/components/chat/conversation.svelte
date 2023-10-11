6609a957967990b12d8ea4eae1107cac5a90a37e
Perramant <perramant.work@gmail.com>
2023-10-11 16:51:37 +0200

    doing the lastmessage content author and hour css

--- src/components/chat/conversation.svelte
+++ src/components/chat/conversation.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let avatar = 'https://cdn.intra.42.fr/users/f1bc7681cd8be9d098d36bb8ac9eb702/jperrama.jpg', name = 'pascal', lastmessage_author = 'Vous', lastmessage_content = 'si c vrai bien vu', lastmessae_hour = '1 h';
+    export let avatar = 'https://cdn.intra.42.fr/users/f1bc7681cd8be9d098d36bb8ac9eb702/jperrama.jpg', name = 'pascal', lastmessage_author = 'Vous', lastmessage_content = 'si c vrai bien vu', lastmessage_hour = '1 h';
 </script>
 
 <div class="flex flex-row h-full w-full">
@@ -8,6 +8,7 @@
     </div>
     <div class="w-[50%] h-full border border-black flex flex-col justify-center">
         <h1>{name}</h1>
+        <h1>{lastmessage_author}: {lastmessage_content} . {lastmessage_hour}</h1>
     </div>
     <div class="w-[20%] h-full">
 
