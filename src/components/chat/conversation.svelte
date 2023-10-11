68d6e7497c8f7d48ac2468a85dd0924da9326cb3
Perramant <perramant.work@gmail.com>
2023-10-11 16:56:15 +0200

    now its good

--- src/components/chat/conversation.svelte
+++ src/components/chat/conversation.svelte
@@ -11,8 +11,8 @@
 		<img src={avatar} alt="avatar" class="pl-2 rounded-full" width="70px" height="70px" />
 	</div>
 	<div class="w-[50%] h-full border border-black flex flex-col justify-center">
-		<h1>{name}</h1>
-		<h1>{lastmessage_author}: {lastmessage_content} . {lastmessage_hour}</h1>
+		<h1 class="font-medium text-[16px]">{name}</h1>
+		<p class="text-[#737373] text-[12px]">{lastmessage_author}: {lastmessage_content} . {lastmessage_hour}</p>
 	</div>
 	<div class="w-[20%] h-full" />
 </div>
