8e0cc01eb8f48482277e78cd6f79ab2be8e06086
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-13 21:43:10 +0200

    added the ctrl K on the search bar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -7,7 +7,7 @@
 				<div
 					class="flex items-center px-6 py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0"
 				>
-					<div class="w-full">
+					<div class="w-full relative">
 						<label for="search" class="sr-only">Search</label>
 						<div class="relative">
 							<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -25,12 +25,13 @@
 								</svg>
 							</div>
 							<input
-								id="search"
-								name="search"
-								class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-								placeholder="Search"
-								type="search"
+							id="search"
+							name="search"
+							class="block w-full rounded-md pr-16 border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+							placeholder="Search"
+							type="search"
 							/>
+							<kbd class="absolute inset-y-0 right-3 top-1/2 transform -translate-y-1/2 rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">⌘K</kbd>
 						</div>
 					</div>
 				</div>
