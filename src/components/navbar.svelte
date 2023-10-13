e3cf7c932f076fdfa97d77fd45c56cd8961a1551
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-14 01:17:28 +0200

    done for the setting page and its implementation

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -164,7 +164,7 @@
 								id="user-menu-item-0">Your Profile</a
 							>
 							<a
-								href="#"
+								href="/settings"
 								class="block px-4 py-2 text-sm text-gray-700"
 								role="menuitem"
 								tabindex="-1"
