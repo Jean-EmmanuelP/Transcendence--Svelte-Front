5b8371d2489b502a46d4cc0512ce78e40b35105d
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-13 21:08:53 +0200

    doing the setting page

---
+++ src/components/setting.svelte
@@ -0,0 +1,171 @@
+<div class="w-full h-full overflow-auto">
+	<div
+		class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8"
+	>
+		<div>
+			<h2 class="text-base font-semibold leading-7">Personal Information</h2>
+			<p class="mt-1 text-sm leading-6 text-gray-400">
+				Here is where you can change your information
+			</p>
+		</div>
+
+		<form class="md:col-span-2">
+			<div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+				<div class="col-span-full flex items-center gap-x-8">
+					<img
+						src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+						alt=""
+						class="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover"
+					/>
+					<div>
+						<button
+							type="button"
+							class="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-white/20"
+							>Change avatar</button
+						>
+						<p class="mt-2 text-xs leading-5 text-gray-400">JPG, GIF or PNG. 1MB max.</p>
+					</div>
+				</div>
+				<div class="col-span-full">
+                    <label for="email" class="block text-sm font-medium leading-6">Email</label>
+					<div class="mt-2">
+						<div
+							class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+						>
+							<input
+								type="text"
+								name="email"
+								id="email"
+								class="flex-1 border-0 bg-transparent py-1.5 pl-1 focus:ring-0 sm:text-sm sm:leading-6"
+								placeholder="janesmith@gmail.com"
+                                readonly
+							/>
+						</div>
+					</div>
+					<label for="pseudo" class="block text-sm font-medium leading-6">Pseudo</label>
+					<div class="mt-2">
+						<div
+							class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+						>
+							<input
+								type="text"
+								name="pseudo"
+								id="pseudo"
+								class="flex-1 border-0 bg-transparent py-1.5 pl-1 focus:ring-0 sm:text-sm sm:leading-6"
+								placeholder="janesmith"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-span-full">
+					<label for="timezone" class="block text-sm font-medium leading-6">Timezone</label>
+					<div class="mt-2">
+						<select
+							id="timezone"
+							name="timezone"
+							class="block w-full rounded-md border-0 bg-white/5 py-1.5 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+						>
+							<option>Pacific Standard Time</option>
+							<option>Eastern Standard Time</option>
+							<option>Greenwich Mean Time</option>
+						</select>
+					</div>
+				</div>
+			</div>
+
+			<div class="mt-8 flex">
+				<button
+					type="submit"
+					class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+					>Save</button
+				>
+			</div>
+		</form>
+	</div>
+
+	<div
+		class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8"
+	>
+		<div>
+			<h2 class="text-base font-semibold leading-7">Change password</h2>
+			<p class="mt-1 text-sm leading-6 text-gray-400">
+				Update your password associated with your account.
+			</p>
+		</div>
+
+		<form class="md:col-span-2">
+			<div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+				<div class="col-span-full">
+					<label for="current-password" class="block text-sm font-medium leading-6"
+						>Current password</label
+					>
+					<div class="mt-2">
+						<input
+							id="current-password"
+							name="current_password"
+							type="password"
+							autocomplete="current-password"
+							class="block w-full rounded-md border-0 bg-white/5 py-1.5 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+				<div class="col-span-full">
+					<label for="new-password" class="block text-sm font-medium leading-6">New password</label>
+					<div class="mt-2">
+						<input
+							id="new-password"
+							name="new_password"
+							type="password"
+							autocomplete="new-password"
+							class="block w-full rounded-md border-0 bg-white/5 py-1.5 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+				<div class="col-span-full">
+					<label for="confirm-password" class="block text-sm font-medium leading-6"
+						>Confirm password</label
+					>
+					<div class="mt-2">
+						<input
+							id="confirm-password"
+							name="confirm_password"
+							type="password"
+							autocomplete="new-password"
+							class="block w-full rounded-md border-0 bg-white/5 py-1.5 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div class="mt-8 flex">
+				<button
+					type="submit"
+					class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+					>Save</button
+				>
+			</div>
+		</form>
+	</div>
+	<div
+		class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8"
+	>
+		<div>
+			<h2 class="text-base font-semibold leading-7">Delete account</h2>
+			<p class="mt-1 text-sm leading-6 text-gray-400">
+				No longer want to use our service? You can delete your account here. This action is not
+				reversible. All information related to this account will be deleted permanently.
+			</p>
+		</div>
+
+		<form class="flex items-start md:col-span-2">
+			<button
+				type="submit"
+				class="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-red-400"
+				>Yes, delete my account</button
+			>
+		</form>
+	</div>
+</div>
