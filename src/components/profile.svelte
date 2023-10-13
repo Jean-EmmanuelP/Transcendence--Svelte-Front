51938f865d0f11f786895e13b43d72a01ef79cbb
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-14 01:56:22 +0200

    added some style and other things

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,23 +1,47 @@
 <script>
 </script>
 
-<div class="w-full h-full">
+<div class="w-full h-full text-white">
 	<div
 		class="flex items-center h-1/3 w-full bg-[url(https://cdn.aglty.io/fortanix/web3/bg.svg)] bg-center bg-cover bg-no-repeat"
 	>
-    <div class="h-full w-1/3 flex items-center justify-center border border-black">
-        <img  class="h-48 w-48 rounded-full" src="https://cdn.intra.42.fr/users/f1bc7681cd8be9d098d36bb8ac9eb702/jperrama.jpg" alt="">
-    </div>
-    <div class="h-full w-1/3 flex items-center justify-center border border-black"></div>
-    <div class="h-full w-1/3 flex items-center justify-center border border-black"></div>
-    </div>
-	<div class="h-1/3 w-full">
+		<div class="h-full w-1/4 flex items-center justify-center border border-black">
+			<img
+				class="h-48 w-48 rounded-full shadow-md"
+				src="https://cdn.intra.42.fr/users/f1bc7681cd8be9d098d36bb8ac9eb702/jperrama.jpg"
+				alt=""
+			/>
+		</div>
+		<div class="flex flex-col h-full w-2/4 flex items-center justify-center border border-black">
+			<div class="flex items-center h-1/3 w-full border border-black">
+				<div class="w-1/2 h-full flex items-center justify-center border border-black">
+					Jean-Emmanuel<br />Perramant
+				</div>
+				<div class="w-1/2 h-full flex items-center justify-center">jperrama</div>
+			</div>
+			<div class="flex flex-col items-center gap-2 h-2/3 w-full p-2">
+				<div class="h-1/4 w-full bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md" />
+				<div class="h-2/4 w-full flex gap-4">
+					<div
+						class="h-full w-1/2 bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md flex flex-col gap-2 items-center justify-center"
+					>
+						<p>ONLINE</p>
+						<p>-</p>
+					</div>
+					<div class="h-full w-1/2 bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md" />
+				</div>
+				<div class="h-1/4 w-full bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md" />
+			</div>
+		</div>
+		<div class="h-full w-1/4 flex items-center justify-center border border-black" />
+	</div>
+	<div class="h-1/3 w-full text-black">
 		<p>
 			Stats (such as: wins and losses, ladder level, achievements, and so forth) have to be
 			displayed on the user profile.
 		</p>
 	</div>
-	<div class="h-1/3 w-full">
+	<div class="h-1/3 w-full text-black">
 		<p>
 			Each user should have a Match History including 1v1 games, ladder, and anything else useful.
 			Anyone who is logged in should be able to consult it.
