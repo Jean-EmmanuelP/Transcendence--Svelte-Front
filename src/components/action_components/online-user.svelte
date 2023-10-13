e9580bfce4ac67c1d5617713b899590938d11465
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-13 22:23:46 +0200

    status props done

--- src/components/action_components/online-user.svelte
+++ src/components/action_components/online-user.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	export let status: "ONLINE": "OFFLINE" = "OFFLINE";
+</script>
+
 <span class="relative inline-block">
 	<img
 		class="h-16 w-16 rounded-full"
@@ -5,6 +9,8 @@
 		alt=""
 	/>
 	<span
-		class="absolute bottom-0 right-0 block h-4 w-4 rounded-full bg-green-400 ring-2 ring-white"
+		class={`absolute bottom-0 right-0 block h-4 w-4 rounded-full ${
+			status === 'ONLINE' ? 'bg-green-400' : 'bg-red-400'
+		} ring-2 ring-white`}
 	/>
 </span>
