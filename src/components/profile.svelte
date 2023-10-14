8c9e831a270d5d8f045eee45f6256e9d50426f9e
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-14 02:04:43 +0200

    done for the level, online and wins - loose

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -28,9 +28,19 @@
 						<p>ONLINE</p>
 						<p>-</p>
 					</div>
-					<div class="h-full w-1/2 bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md" />
+					<div
+						class="flex flex-col gap-2 items-center justify-center h-full w-1/2 bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md"
+					>
+						<h1>WINS - LOOSE</h1>
+						<p>00 - 00</p>
+					</div>
 				</div>
-				<div class="h-1/4 w-full bg-gradient-to-r from-[#4F46E5] to-blue-500/90 rounded-md" />
+				<div
+					class="h-1/4 w-full bg-gradient-to-r from-black to-black/80 rounded-md flex items-center justify-center relative"
+				>
+					<div class="absolute inset-y-0 left-0 bg-indigo-600 rounded-l-md w-[45%] h-full" />
+					<span class="z-10">level 7 - 45%</span>
+				</div>
 			</div>
 		</div>
 		<div class="h-full w-1/4 flex items-center justify-center border border-black" />
