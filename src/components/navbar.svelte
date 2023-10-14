877e7642430dbec6b5b5adc4660f039d01040df7
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-14 18:18:49 +0200

    added the profile in the navabr

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -157,7 +157,7 @@
 							out:fly={{ y: -20, duration: 300 }}
 						>
 							<a
-								href="#"
+								href="/profile"
 								class="block px-4 py-2 text-sm text-gray-700"
 								role="menuitem"
 								tabindex="-1"
