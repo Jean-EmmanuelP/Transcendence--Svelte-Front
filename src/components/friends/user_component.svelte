1e6b324a3dd737a610ff747c791959610dd7f85d
Tselmeg ADIYAMUNKH <tadiyamu@made-f0Br5s8.clusters.42paris.fr>
2023-10-15 15:52:10 +0200

    Add friend

---
+++ src/components/friends/user_component.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { sendFriendRequest } from '../../services/gqlFriends';
+	import type { AuthenticationType } from '../../stores/authentication';
+	import { afterUpdate } from 'svelte';
+	export let user: AuthenticationType;
+	export let handleSent: any;
+
+	let loading = false;
+	async function simulateAsyncAction() {
+		loading = true;
+		// Simulating an asynchronous action
+		try {
+			const result = await sendFriendRequest(user.pseudo);
+			console.log(result);
+			handleSent(user.pseudo);
+			loading = false;
+		} catch (e) {
+			loading = false;
+		}
+	}
+
+	function handleButtonClick() {
+		if (!loading) {
+			simulateAsyncAction();
+		}
+	}
+
+
+</script>
+
+<div class="flex w-full justify-between mb-3 content-center">
+	<div class="flex items-center space-x-4">
+		<img
+			class="w-10 h-10 rounded-full"
+			src="https://api.dicebear.com/7.x/bottts/svg?seed={user.pseudo}"
+			alt=""
+		/>
+		<div class="font-medium">
+			<div>{user.name}</div>
+			<div class="text-sm text-gray-500">{user.pseudo}</div>
+		</div>
+	</div>
+	<button
+		type="button"
+		class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+		on:click={handleButtonClick}
+		disabled={loading}
+	>
+		{#if loading}
+			<svg
+				class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+				<path
+					class="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+				/>
+			</svg>
+			Loading
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
+			</svg>
+			<span class="sr-only">Icon description</span>
+		{/if}
+	</button>
+</div>
---
+++ src/services/gqlFriends.ts
@@ -0,0 +1,69 @@
+import client from './apolloClient';
+import { gql } from '@apollo/client/core/index.js';
+
+
+export const getUsers = async () => {
+	try {
+		const response = await client.query({
+			query: gql`
+				{
+					users {
+						id
+						email
+						name
+						pseudo
+						avatar
+						status
+					}
+				}
+			`
+		});
+		return response.data.users;
+	} catch (error) {
+		console.log(error);
+		throw new Error('Error fetching user information');
+	}
+};
+
+export const getFriends = async () => {
+	try {
+		const response = await client.query({
+			query: gql`
+				{
+					getAllFriendsOfUser {
+						id
+						email
+						name
+						pseudo
+						avatar
+						isTwoFactorEnabled
+						status
+					}
+				}
+			`
+		});
+		return response.data.getAllFriendsOfUser;
+	} catch (error) {
+		console.log(error);
+		throw new Error('Error fetching user information');
+	}
+};
+
+export const sendFriendRequest = async (pseudo: string) => {
+	try {
+		const response = await client.mutate({
+			mutation: gql`
+				mutation sendFriendRequest($pseudo: String!) {
+					sendFriendRequest(receiverPseudo: $pseudo)
+				}
+			`,
+			variables: {
+				pseudo: pseudo
+			}
+		});
+		return response.data.sendFriendRequest;
+	} catch (error) {
+		console.log(error);
+		throw new Error('Error updating user pseudo');
+	}
+};
