928aa8ec0a5a6cb5e4cb9d7b7fd39bfca7aecf41
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-15 16:28:14 +0200

    changed the name of the handleMutationPseudo

--- src/components/setting.svelte
+++ src/components/setting.svelte
@@ -19,7 +19,7 @@
 	function handle2FAClick() {
 		enabled = !enabled;
 	}
-	async function handleMutation() {
+	async function handleMutationPseudo() {
 		try {
 			await updateUserPseudo($pseudo);
 			authentication.update(val => ({ ...val, pseudo: $pseudo }));
@@ -111,7 +111,7 @@
 				<button
 					type="button"
 					class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-					on:click={handleMutation}>Save</button
+					on:click={handleMutationPseudo}>Save</button
 				>
 			</div>
 		</form>
