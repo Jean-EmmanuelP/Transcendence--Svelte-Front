bac9f41d99dab3aab609c3512fa2f78533f69fd0
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 20:39:45 +0200

    depending of menutop and menuleft

--- src/components/modal_wrapper.svelte
+++ src/components/modal_wrapper.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
 	export let isOpen = false;
-	export let menuTop : null | number = null, menuLeft: null | number = null;
+	export let menuTop: null | number = null,
+		menuLeft: null | number = null;
 </script>
 
 {#if isOpen}
 	<div class="fixed top-0 left-0 w-screen h-screen" on:click={() => (isOpen = false)}>
 		<div
 			transition:fly={{ y: 200, duration: 200 }}
-			class={`absolute top-[${menuTop}px] left-[${menuLeft}px]]`}
+			class={`absolute ${menuTop && menuLeft ? `top-[${menuTop}px] left-[${menuLeft}px]` : ''}`}
 			on:click={(e) => e.stopPropagation()}
 		>
 			<slot />
