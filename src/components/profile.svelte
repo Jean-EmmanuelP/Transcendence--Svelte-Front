e05535296a8bd735ab17977bea551ffc9aa51e96
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 11:16:03 +0200

    structuring the profile page

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -15,10 +15,13 @@
 </script>
 
 <div class="flex flex-col h-full w-full border border-black">
-	<div class="w-full h-[60%] border border-black">
-		<img src={user.avatar} class="rounded-full" alt="avatar picture" />
-		<h1>{user.status}</h1>
-		
+	<div class="w-full h-[60%] border border-black flex items-center flex-col mt-2">
+		<img src={user.avatar} class="rounded-full w-48 h-48 mb-2 shadow-md" alt="avatar picture" />
+		<h1 class="font-bold">{user.status}</h1>
+		<div class="flex w-full flex flex-col items-center justify-center mt-2">
+			<p class="font-medium">Level 7</p>
+			<div class="mt-2 rounded-md h-3 w-[60%] border border-gray-500/15 shadow-sm" />
+		</div>
 	</div>
 	<div class="w-full h-[40%] flex flex-row">
 		<GameStats />
