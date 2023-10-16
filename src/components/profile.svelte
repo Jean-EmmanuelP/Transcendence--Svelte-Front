6f43fe0bbc39089a61ffcbf6d64ff31676455710
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 10:59:20 +0200

    commit changes before installing shadcnui

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { onDestroy } from 'svelte';
 	import { authentication, type AuthenticationType } from '../stores/authentication';
+	import GameStats from '$components/statistics/game_stats.svelte';
+
+	import AchievmentsStats from '$components/statistics/achievments_stats.svelte';
 
 	let user: AuthenticationType;
 	const onsubscribe = authentication.subscribe((value) => {
@@ -11,6 +14,14 @@
 	// mutation
 </script>
 
-<div class="h-full w-full">
-		<img src={user.avatar} class="rounded-full" alt="avatar picture">
+<div class="flex flex-col h-full w-full border border-black">
+	<div class="w-full h-[60%] border border-black">
+		<img src={user.avatar} class="rounded-full" alt="avatar picture" />
+		<h1>{user.status}</h1>
+		
+	</div>
+	<div class="w-full h-[40%] flex flex-row">
+		<GameStats />
+		<AchievmentsStats />
+	</div>
 </div>
