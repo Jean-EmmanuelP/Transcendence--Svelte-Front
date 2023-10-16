b33d3392ad3c70eb9a64885ad3b18c6c58c989c6
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 09:32:05 +0200

    did the card for stats

--- src/components/statistics/stats.svelte
+++ src/components/statistics/stats.svelte
@@ -5,6 +5,13 @@
             <dt class="truncate text-sm font-medium text-gray-500">Total Subscribers</dt>
             <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">71,897</dd>
         </div>
-        <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm: p-6"></div>
+        <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+            <dt class="truncate text-sm font-medium text-gray-500">Avg. Open Rate</dt>
+            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">58.16%</dd>
+        </div>
+        <div class="overflow-hiddne rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+            <dt class="truncate text-sm font-medium text-gray-500">Avg.Click Rate</dt>
+            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">24.57%</dd>
+        </div>
     </dl>
 </div>
