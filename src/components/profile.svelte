95acc7c0e5d8726156c948d46650dae39ded6602
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 11:24:45 +0200

    now doing the padding styling removed the border

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -4,7 +4,7 @@
 	import GameStats from '$components/statistics/game_stats.svelte';
 
 	import AchievmentsStats from '$components/statistics/achievments_stats.svelte';
-	import Progress from '$components/action_components/progress.svelte'
+	import Progress from '$components/action_components/progress.svelte';
 
 	let user: AuthenticationType;
 	const onsubscribe = authentication.subscribe((value) => {
@@ -15,16 +15,16 @@
 	// mutation
 </script>
 
-<div class="flex flex-col h-full w-full border border-black">
-	<div class="w-full h-[60%] border border-black flex items-center flex-col mt-2">
+<div class="flex flex-col h-full w-full">
+	<div class="w-full h-[53%] flex items-center flex-col mt-2">
 		<img src={user.avatar} class="rounded-full w-48 h-48 mb-2 shadow-md" alt="avatar picture" />
 		<h1 class="font-bold">{user.status}</h1>
 		<div class="flex w-full flex flex-col items-center justify-center mt-2">
 			<p class="font-medium">Level 7</p>
-			<Progress progress=30 />
+			<Progress progress="30" />
 		</div>
 	</div>
-	<div class="w-full h-[40%] flex flex-row">
+	<div class="w-full h-[47%] flex flex-row items-center justify-center gap-5">
 		<GameStats />
 		<AchievmentsStats />
 	</div>
