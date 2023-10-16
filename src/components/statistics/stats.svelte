41864057df35db5c702dfd054d2e471168fc9b84
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 09:26:29 +0200

    doing the stats for svelte component

---
+++ src/components/statistics/stats.svelte
@@ -0,0 +1,10 @@
+<div>
+    <h3 class="text-base font-semibold leading-6 text-gray-900">Last 30 days</h3>
+    <dl class="mt-5 grid gird-cols-1 gap-5 sm:grid-cols-3">
+        <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+            <dt class="truncate text-sm font-medium text-gray-500">Total Subscribers</dt>
+            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">71,897</dd>
+        </div>
+        <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm: p-6"></div>
+    </dl>
+</div>
