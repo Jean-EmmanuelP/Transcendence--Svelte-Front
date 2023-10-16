c52586524572e92c73f683e2176da5203e93d68d
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 19:53:20 +0200

    done the modal wrapper and linked the sore modal to the local isUserMenuOpen and this is reactive by the change that are done in the modalwrapper

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -6,16 +6,23 @@
 	import ModalWrapper from '$components/modal_wrapper.svelte';
 
 	let user: AuthenticationType;
+	let isUserMenuOpen = false;
+
 	const unsubscribe = authentication.subscribe((value) => {
 		user = value;
 	});
 	onDestroy(unsubscribe);
 
+	$: isUserMenuOpen = $modalOpen === 'userMenu';
+	$: if (!isUserMenuOpen && $modalOpen === 'userMenu') {
+		modalOpen.set(null);
+	}
+
 	function toggleModal() {
 		if ($modalOpen) {
 			modalOpen.set(null);
 		} else {
-			modalOpen.set('userMenu')
+			modalOpen.set('userMenu');
 		}
 	}
 </script>
@@ -154,7 +161,7 @@
                 From: "transform opacity-100 scale-100"
                 To: "transform opacity-0 scale-95"
             -->
-					<ModalWrapper bind:isOpen={$modalOpen === 'userMenu'}>
+					<ModalWrapper bind:isOpen={isUserMenuOpen}>
 						<div
 							class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 							role="menu"
