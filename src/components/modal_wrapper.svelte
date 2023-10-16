195b7a2891c79a15f2a72fa5d3d5c7ee2b3db417
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 19:34:23 +0200

    modal wrapper done

---
+++ src/components/modal_wrapper.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	export let isOpen = false;
+</script>
+
+{#if isOpen}
+	<div class="" on:click={() => (isOpen = false)}>
+		<div transition:fly={{ y: 200, duration: 200 }}>
+			<slot />
+		</div>
+	</div>
+{/if}
