547d44bcf80a0a96c70576efb1d83e355ab488b7
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 11:22:16 +0200

    progress bar doing the ui

--- src/components/action_components/progress.svelte
+++ src/components/action_components/progress.svelte
@@ -2,6 +2,6 @@
     export let progress: number = 0;
 </script>
 
-<div class={`mt-2 rounded-md h-3 w-[60%] border border-gray-500/15 shadow-sm`}>
-    <div class={`h-full w-[${progress}%] bg-blue-500`} />
+<div class={`mt-2 rounded-lg h-3 w-[60%] border bg-gray-500/10 border-gray-500/15 shadow-sm`}>
+    <div class={`rounded-l-lg h-full w-[${progress}%] bg-black`} />
 </div>
