79f1cefd2067195fab771468c211e44c627bf3d5
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 09:51:23 +0200

    added the hr

--- src/routes/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9/stats/+page.svelte
+++ src/routes/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9/stats/+page.svelte
@@ -1,5 +1,10 @@
 <script>
-    import Stats from "$components/statistics/achievments_stats.svelte";
+    import GameStats from "$components/statistics/game_stats.svelte";
+	import AchievmentsStats from "$components/statistics/achievments_stats.svelte";
 </script>
 
-<Stats />
+<div class="flex gap-5 flex-col">
+    <AchievmentsStats />
+    <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
+    <GameStats />
+</div>
