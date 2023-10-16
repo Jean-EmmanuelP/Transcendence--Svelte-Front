4c0436513d14bcd77e3e8b49f1c9e5f26fdecd10
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-16 11:34:02 +0200

    done for the hover transition for the game

--- src/components/statistics/game_stats.svelte
+++ src/components/statistics/game_stats.svelte
@@ -7,15 +7,15 @@
 <div>
 	<!-- <h3 class="text-base font-semibold leading-6 text-gray-900">Games stat's</h3> -->
 	<dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6 transition transition-transform duration-300 hover:scale-105 hover:shadow-lg">
 			<dt class="truncate text-sm font-medium text-gray-500">Total Match</dt>
 			<dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{total_games}</dd>
 		</div>
-		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6 transition transition-transform duration-300 hover:scale-105 hover:shadow-lg">
 			<dt class="truncate text-sm font-medium text-gray-500">Avg. Open Rate</dt>
 			<dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{win_ratio}</dd>
 		</div>
-		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6 transition transition-transform duration-300 hover:scale-105 hover:shadow-lg">
 			<dt class="truncate text-sm font-medium text-gray-500">Avg. Click Rate</dt>
 			<dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{lose_ratio}</dd>
 		</div>
