8c09debf564f6599bf96825bc7c80bbc918309a5
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-17 19:29:58 +0200

    navbar ring

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -63,7 +63,7 @@
 	}
 </script>
 
-<header class="bg-white shadow-sm lg:static lg:overflow-y-visible">
+<header class="bg-white shadow-sm ring-1 ring-gray-200/20 lg:static lg:overflow-y-visible">
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 		<div class="relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12">
 			<div class="flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2" />
