8bc6394434b4206fc5381d2361f95831fa2f5135
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-18 23:29:04 +0200

    now the profile is an image

--- src/components/sidebar/sidebar.svelte
+++ src/components/sidebar/sidebar.svelte
@@ -126,25 +126,12 @@
 								sidebaricon === 'icon' && 'items-center justify-center'
 							} rounded-md p-2 text-sm leading-6 font-semibold`}
 						>
-							<svg
-								class="h-6 w-6 shrink-0 text-black group-hover:text-black"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-								/>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-								/>
-							</svg>
+							<img
+								src={user.avatar}
+								class="rounded-full h-6 w-6 shrink-0 text-black group-hover:text-black"
+								alt={user.name}
+							/>
+
 							{#if sidebaricon === 'normal'}
 								<p>Profile</p>
 							{/if}
