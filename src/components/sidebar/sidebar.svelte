0446be39fad4cd74fa7aeea601ff17b6356c046d
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-18 23:26:31 +0200

    replaced the message logo to instagram lgoo

--- src/components/sidebar/sidebar.svelte
+++ src/components/sidebar/sidebar.svelte
@@ -84,19 +84,31 @@
 							} gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`}
 						>
 							<svg
+								aria-label="Direct"
 								class="h-6 w-6 shrink-0 text-black group-hover:text-black"
-								fill="none"
+								color="rgb(0, 0, 0)"
+								fill="rgb(0, 0, 0)"
+								height="24"
+								role="img"
 								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
+								width="24"
+								><title>Direct</title><line
+									fill="none"
+									stroke="currentColor"
 									stroke-linejoin="round"
-									d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-								/>
-							</svg>
+									stroke-width="2"
+									x1="22"
+									x2="9.218"
+									y1="3"
+									y2="10.083"
+								/><polygon
+									fill="none"
+									points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+									stroke="currentColor"
+									stroke-linejoin="round"
+									stroke-width="2"
+								/></svg
+							>
 							{#if sidebaricon === 'normal'}
 								<p>Messages</p>
 							{/if}
