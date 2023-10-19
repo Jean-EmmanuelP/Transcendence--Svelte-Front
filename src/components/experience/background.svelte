d1938d5668baf39c9977f679a1d4d8095cbc2850
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-19 17:50:58 +0200

    trying things with the play button

--- src/components/experience/background.svelte
+++ src/components/experience/background.svelte
@@ -1,5 +1,30 @@
 <div class="paddle h-full w-full flex items-center justify-center">
-		<div class="card">
-
-        </div>
+	<div class="card">
+		<div class="relative h-full w-full">
+			<div
+				class="absolute -inset-0.5 bg-pink-600 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg opacity-75 group-hover: opacity-100 transition duration-1000 grouyp-hover:duration-200 animate-tilt"
+			/>
+			<button
+				class="relative px-7 py-4 bg-black rounded-lg leading-none divide-x divide-gray-600 flex"
+			>
+				<span class="flex items-center space-x-5">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6 text-pink-600 -rotate-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+						/>
+					</svg>
+					<span class="pr-6 text-gray-100">Play</span>
+				</span>
+			</button>
+		</div>
+	</div>
 </div>
