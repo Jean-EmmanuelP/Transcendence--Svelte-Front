e3129a4571213c82bc90d22bbe3d89cbbf08b368
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-19 14:40:05 +0200

    added in the tailwind config the tilt effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        tilt: 'tilt 10s infinite linear'
+      },
+      keyframes: {
+        tilt: {
+          "0% 50% 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(5deg)",
+          },
+          "75%": {
+            transform: "rotate(-5deg)",
+          },
+        }
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
