c7521db5bfdcd38a0de04173771bc9568cc7deec
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-19 18:56:39 +0200

    trying with if else depending of the value of clickedplay

--- src/components/experience/background.svelte
+++ src/components/experience/background.svelte
@@ -16,41 +16,44 @@
 				<div
 					class="absolute -inset-0.5 bg-gradient-to-r from-black to-black rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
 				/>
-				<button
-					on:click={clickedPlayButton}
-					in:fade={{ delay: 100, duration: 500 }}
-					class={`group relative px-7 py-4 bg-white rounded-lg leading-none flex transition duration-500 transform ease-in-out ${
-						clickedPlay ? 'opacity-0' : 'opacity-100'
-					}`}
-				>
-					<span class="flex items-center space-x-5">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6 -rotate-6 text-black group-hover:text-black transition transform duration-1000 group-hover:duration-500"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-							/>
-						</svg>
-						<span class="pr-6 tracking-wide text-black font-poppins group-hover:font-bold"
-							>Play</span
-						>
-					</span>
-				</button>
-				<div
-					class={`flex flex-col items-center justify-center gap-2 h-full w-full transition duration-500 transform ease-in-out ${
-						clickedPlay ? 'opacity-100' : `opacity-0`
-					}`}
-				>
-					<button class="rounded-md bg-white z-10 text-black p-4">boutton 1</button>
-					<button class="rounded-md bg-white z-10 text-black p-4">boutton 2</button>
-				</div>
+				{#if !clickedPlay}
+					<button
+						on:click={clickedPlayButton}
+						in:fade={{ delay: 100, duration: 500 }}
+						class={`group relative px-7 py-4 bg-white rounded-lg leading-none flex transition duration-500 transform ease-in-out ${
+							clickedPlay ? 'opacity-0' : 'opacity-100'
+						}`}
+					>
+						<span class="flex items-center space-x-5">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6 -rotate-6 text-black group-hover:text-black transition transform duration-1000 group-hover:duration-500"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+								/>
+							</svg>
+							<span class="pr-6 tracking-wide text-black font-poppins group-hover:font-bold"
+								>Play</span
+							>
+						</span>
+					</button>
+				{:else}
+					<div
+						class={`flex flex-col items-center justify-center gap-2 h-full w-full transition duration-500 transform ease-in-out ${
+							clickedPlay ? 'opacity-100' : `opacity-0`
+						}`}
+					>
+						<button class="rounded-md bg-white z-10 text-black p-4">boutton 1</button>
+						<button class="rounded-md bg-white z-10 text-black p-4">boutton 2</button>
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
