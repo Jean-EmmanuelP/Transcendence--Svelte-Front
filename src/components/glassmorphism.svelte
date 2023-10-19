d1494b750e40b77d777c457d3ca6b08198877a92
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-19 16:40:32 +0200

    doing the glassmorphism project mastercard project to learn this

--- src/components/glassmorphism.svelte
+++ src/components/glassmorphism.svelte
@@ -1,3 +1,39 @@
-<div class="w-full h-full flex items-center justify-center">
-    glassmorphism
+<div class="bg-[#121321] w-full h-full flex items-center justify-center">
+	<!-- Color Circle -->
+	<div class="circle">
+		<!-- Whole Card Section -->
+		<div class="card text-white tracking-[1px]">
+			<!-- Card header section -->
+			<header class="flex justify-between items-center m-[20px]">
+				<!-- master card logo -->
+				<span class="w-[40px] h-[40px]">
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Mastercard-logo.svg/1280px-Mastercard-logo.svg.png"
+						alt=""
+					/>
+				</span>
+				<!-- master card name -->
+				<h1 class="font-poppins font-[16px] -ml-[90px]">Master Card</h1>
+				<!-- master sim chip -->
+				<span class="w-[40px] h-[40px]">
+					<img
+						src="https://cdn.freebiesupply.com/logos/large/2x/chip-1-logo-png-transparent.png"
+						alt=""
+					/>
+				</span>
+			</header>
+			<!-- card details -->
+			<div class="flex justify-between m-[25px] uppercase">
+				<div>
+					<h1 class="text-[10px]">card number</h1>
+					<h1>4021 3204 1323 1323</h1>
+					<h1>ziu hoque</h1>
+				</div>
+				<div class="mt-[25px]">
+					<h1 class="text-[10px]">expires on</h1>
+                    <h1>05/23</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      animation: {
-        tilt: 'tilt 10s infinite linear'
-      },
-      keyframes: {
-        tilt: {
-          "0% 50% 100%": {
-            transform: "rotate(0deg)",
-          },
-          "25%": {
-            transform: "rotate(1deg)",
-          },
-          "75%": {
-            transform: "rotate(-1deg)",
-          },
-        }
-      }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ]
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			poppins: ['Poppins', 'sans-serif']
+		},
+		extend: {
+			animation: {
+				tilt: 'tilt 10s infinite linear'
+			},
+			keyframes: {
+				tilt: {
+					'0% 50% 100%': {
+						transform: 'rotate(0deg)'
+					},
+					'25%': {
+						transform: 'rotate(1deg)'
+					},
+					'75%': {
+						transform: 'rotate(-1deg)'
+					}
+				}
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/forms')]
 };
