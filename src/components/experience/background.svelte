d8f766a484a45221010a0c881e5380e7e45bdd7e
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-19 17:59:59 +0200

    doing the button for the play

--- src/components/experience/background.svelte
+++ src/components/experience/background.svelte
@@ -1,30 +1,36 @@
 <div class="paddle h-full w-full flex items-center justify-center">
-	<div class="relative h-[80%] w-[80%] bg-white/10 z-10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl">
-		<div class="relative h-full w-full">
-			<div
-				class="absolute -inset-0.5 bg-pink-600 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg opacity-75 group-hover: opacity-100 transition duration-1000 grouyp-hover:duration-200 animate-tilt"
-			/>
-			<button
-				class="relative px-7 py-4 bg-black rounded-lg leading-none divide-x divide-gray-600 flex"
-			>
-				<span class="flex items-center space-x-5">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6 text-pink-600 -rotate-6"
+	<div
+		class="h-[80%] w-[80%] bg-white/10 z-10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl"
+	>
+		<div class="w-full h-full flex items-center justify-center transparent">
+			<div class="relative group">
+				<div
+					class="absolute -inset-0.5 bg-blue-600 bg-gradient-to-r from-blue-600 to-red-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"
+				/>
+				<button class="relative px-7 py-4 bg-white rounded-lg leading-none flex">
+					<span class="flex items-center space-x-5">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6 -rotate-6 group-hover:rotate-0 transition transform duration-300 group-hover:duration-150"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+							/>
+						</svg>
+
+						<span class="pr-6 text-gray-100">Labs Release 2021.09</span>
+					</span>
+					<span class="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200"
+						>See what's new &rarr;</span
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-						/>
-					</svg>
-					<span class="pr-6 text-gray-100">Play</span>
-				</span>
-			</button>
+				</button>
+			</div>
 		</div>
 	</div>
 </div>
