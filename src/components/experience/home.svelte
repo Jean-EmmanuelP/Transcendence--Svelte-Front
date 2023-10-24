4a8c08df8d20b5f01aad1f3ba7a9450fe6224db4
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-25 00:12:01 +0200

    spin around :D

--- src/components/experience/home.svelte
+++ src/components/experience/home.svelte
@@ -184,34 +184,139 @@
 							clickedPlay ? 'opacity-100' : `opacity-100`
 						}`}
 					>
-						<div class="relative flex w-full max-w-[200px] rounded-md ring-1 ring-gray-500/20 group/1">
+						<div
+							class="relative flex w-full max-w-[200px] rounded-md ring-1 ring-gray-500/20 group/1"
+						>
 							<div
 								class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover/1:opacity-75 transition duration-1000 group-hover/1:duration-200 animate-tilt"
 							/>
 							<button
-								class="uppercase relative w-full rounded-md bg-black text-white font-medium p-4  transition transform duration-500 hover:duration-300"
+								class="uppercase relative w-full rounded-md bg-black text-white font-medium p-4 transition transform duration-500 hover:duration-300"
 								on:click={() => clickedButton('matchmaking')}
 							>
 								<span />
 								<p class="tracking-wider font-bold group-hover/1:font-extrabold">Matchmaking</p>
 							</button>
 						</div>
-						<div class="relative flex w-full max-w-[200px] rounded-md ring-1 ring-gray-500/20 group/2">
+						<div
+							class="relative flex w-full max-w-[200px] rounded-md ring-1 ring-gray-500/20 group/2"
+						>
 							<div
 								class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover/2:opacity-75 transition duration-1000 group-hover/2:duration-200 animate-tilt"
 							/>
 							<button
 								class="uppercase relative rounded-md bg-black text-white p-4 font-medium hover:font-bold transition transform duration-500 hover:duration-300"
 								on:click={() => clickedButton('friends')}
-								>
+							>
 								<span />
-								<p class="tracking-wider font-bold group-hover/2:font-extrabold">Play with a friend</p>
+								<p class="tracking-wider font-bold group-hover/2:font-extrabold">
+									Play with a friend
+								</p>
 							</button>
 						</div>
 					</div>
 				{:else if clickedPlay && (clickedPlayWithFriends || clickedMatchmaking)}
-					<div class="text-white">
-						loading spin
+					<div class="text-white flex items-center justify-center w-[200px] h-[200px]">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="200"
+							height="200"
+						viewBox="0 0 24 24"
+							><style>
+								.spinner_EUy1 {
+									animation: spinner_grm3 1.2s infinite;
+									fill: #ffffff;
+								}
+								.spinner_f6oS {
+									animation-delay: 0.1s;
+								}
+								.spinner_g3nX {
+									animation-delay: 0.2s;
+								}
+								.spinner_nvEs {
+									animation-delay: 0.3s;
+								}
+								.spinner_MaNM {
+									animation-delay: 0.4s;
+								}
+								.spinner_4nle {
+									animation-delay: 0.5s;
+								}
+								.spinner_ZETM {
+									animation-delay: 0.6s;
+								}
+								.spinner_HXuO {
+									animation-delay: 0.7s;
+								}
+								.spinner_YaQo {
+									animation-delay: 0.8s;
+								}
+								.spinner_GOx1 {
+									animation-delay: 0.9s;
+								}
+								.spinner_4vv9 {
+									animation-delay: 1s;
+								}
+								.spinner_NTs9 {
+									animation-delay: 1.1s;
+								}
+								.spinner_auJJ {
+									transform-origin: center;
+									animation: spinner_T3O6 6s linear infinite;
+								}
+								@keyframes spinner_grm3 {
+									0%,
+									50% {
+										animation-timing-function: cubic-bezier(0.27, 0.42, 0.37, 0.99);
+										r: 1px;
+									}
+									25% {
+										animation-timing-function: cubic-bezier(0.53, 0, 0.61, 0.73);
+										r: 2px;
+									}
+								}
+								@keyframes spinner_T3O6 {
+									0% {
+										transform: rotate(360deg);
+									}
+									100% {
+										transform: rotate(0deg);
+									}
+								}
+							</style><g class="spinner_auJJ"
+								><circle class="spinner_EUy1" cx="12" cy="3" r="1" /><circle
+									class="spinner_EUy1 spinner_f6oS"
+									cx="16.50"
+									cy="4.21"
+									r="1"
+								/><circle class="spinner_EUy1 spinner_NTs9" cx="7.50" cy="4.21" r="1" /><circle
+									class="spinner_EUy1 spinner_g3nX"
+									cx="19.79"
+									cy="7.50"
+									r="1"
+								/><circle class="spinner_EUy1 spinner_4vv9" cx="4.21" cy="7.50" r="1" /><circle
+									class="spinner_EUy1 spinner_nvEs"
+									cx="21.00"
+									cy="12.00"
+									r="1"
+								/><circle class="spinner_EUy1 spinner_GOx1" cx="3.00" cy="12.00" r="1" /><circle
+									class="spinner_EUy1 spinner_MaNM"
+									cx="19.79"
+									cy="16.50"
+									r="1"
+								/><circle class="spinner_EUy1 spinner_YaQo" cx="4.21" cy="16.50" r="1" /><circle
+									class="spinner_EUy1 spinner_4nle"
+									cx="16.50"
+									cy="19.79"
+									r="1"
+								/><circle class="spinner_EUy1 spinner_HXuO" cx="7.50" cy="19.79" r="1" /><circle
+									class="spinner_EUy1 spinner_ZETM"
+									cx="12"
+									cy="21"
+									r="1"
+								/></g
+							></svg
+						>
 					</div>
 				{/if}
 			</div>
