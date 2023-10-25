2ee1012bcc824b7bbc0a6749dbb658e6dde0278d
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-25 17:35:36 +0200

    the path is working

--- src/components/experience/home.svelte
+++ src/components/experience/home.svelte
@@ -41,6 +41,13 @@
 	// integrer dans cette fonction le fait de pouvoir partir de la queue de matchmaking et de la queue de play with friend
 	function back() {
 		console.log(`Entered here in back method`);
+		if (showDiv) {
+			showDiv = false;
+			clickedPlayWithFriends = false;
+			clickedMatchmaking = false;
+			return;
+		}
+			
 		if (clickedPlay && (clickedPlayWithFriends || clickedMatchmaking)) {
 			clickedPlayWithFriends = false;
 			clickedMatchmaking = false;
@@ -175,7 +182,7 @@
 	<div
 		on:mouseleave={handleMouseLeave}
 		on:mousemove={handleMouseMove}
-		class="h-[80%] w-[80%] z-10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl transition duration-100 bg-black laser-effect"
+		class="h-[80%] w-[80%] z-10 ring-1 ring-white/10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl transition duration-100 bg-black laser-effect"
 		bind:this={laserElement}
 	>
 		<div
@@ -206,7 +213,7 @@
 						}`}
 						bind:this={playElement}
 					/>
-				{:else if clickedPlay && !clickedPlayWithFriends && !clickedMatchmaking}
+				{:else if clickedPlay && !clickedPlayWithFriends && !clickedMatchmaking && !showDiv}
 					<div
 						class={`relative flex flex-col items-center justify-center h-full w-full transition duration-500 transform ease-in-out gap-7 ${
 							clickedPlay ? 'opacity-100' : `opacity-100`
@@ -230,38 +237,6 @@
 								/>
 							</svg>
 						</button>
-						{#if showDiv}
-							<div
-								class="bg-black rounded-md text-white ring-1 ring-gray-600/20 backdrop-blur-2xl p-2 gap-4 flex flex-col shadow-md absolute top-[-50px] right-[-280px] w-[200px] h-[250px] max-h-[250px]"
-							>
-								<div
-									class="absolute -inset-0.5 bg-gradient-to-r from-red-500/30 to-blue-500/30 rounded-lg blur opacity-50 group-hover/1:opacity-75 transition duration-1000 group-hover/1:duration-200 animate-tilt"
-								/>
-								<header
-									class="z-50 w-full h-[15px] text-white font-bold flex items-center justify-center fixed uppercase"
-								>
-									Friends
-								</header>
-								<div class="z-50 no-scrollbar overflow-x-auto flex flex-col gap-4 h-full mt-8">
-									{#each Array(5) as _, index (index)}
-										<button
-											class="border w-full group border-gray-600/10 shadow-lg hover:bg-gray-500/50 ring-1 ring-gray-600/20 rounded-md rounded-l-full h-1/2 flex items-center gap-2"
-											on:click={() => clickedButton('friends', true)}
-										>
-											<img
-												src="https://avatars.githubusercontent.com/u/59157371?v=4"
-												class="rounded-full h-11 w-11 group-hover:border group-hover:border-white/20 group-hover:scale-110"
-												alt=""
-											/>
-											<p class="truncate">Akadil</p>
-											<div class="truncate hidden group-hover:block text-white/30 font-extrabold">
-												Invite
-											</div>
-										</button>
-									{/each}
-								</div>
-							</div>
-						{/if}
 						<div
 							class="relative flex w-full max-w-[200px] rounded-md ring-1 ring-gray-500/20 group/1"
 						>
@@ -291,6 +266,55 @@
 									Play with a friend
 								</p>
 							</button>
+						</div>
+					</div>
+				{:else if showDiv}
+					<button
+						class="absolute top-[-50px] left-[-10px] text-white/40 shadow-sm hover:scale-110 rounded-full"
+						on:click={back}
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+					</button>
+					<div
+						class="rounded-md text-white ring-1 ring-gray-600/20 backdrop-blur-2xl p-2 gap-4 flex flex-col shadow-md absolute top-0 right-0 w-[200px] h-[250px] max-h-[250px]"
+					>
+						<div
+							class="absolute -inset-0.5 bg-gradient-to-r from-red-500/10 to-blue-500/10 rounded-lg blur opacity-50 group-hover/1:opacity-75 transition duration-1000 group-hover/1:duration-200 animate-tilt"
+						/>
+						<header
+							class="z-50 w-full h-[15px] text-white font-bold flex items-center justify-center fixed uppercase"
+						>
+							Friends
+						</header>
+						<div class="z-50 no-scrollbar overflow-x-auto flex flex-col gap-4 h-full mt-8">
+							{#each Array(5) as _, index (index)}
+								<button
+									class="border w-full group border-gray-600/10 shadow-lg hover:bg-gray-500/50 ring-1 ring-gray-600/20 rounded-md rounded-l-full h-1/2 flex items-center gap-2"
+									on:click={() => clickedButton('friends', true)}
+								>
+									<img
+										src="https://avatars.githubusercontent.com/u/59157371?v=4"
+										class="rounded-full h-11 w-11 group-hover:border group-hover:border-white/20 group-hover:scale-110"
+										alt=""
+									/>
+									<p class="truncate">Akadil</p>
+									<div class="truncate hidden group-hover:block text-white/30 font-extrabold">
+										Invite
+									</div>
+								</button>
+							{/each}
 						</div>
 					</div>
 				{:else if clickedPlay && (clickedPlayWithFriends || clickedMatchmaking)}
