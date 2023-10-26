1e1ac55e68c222e5a7c22631f2fd9bd4a5996358
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-26 15:55:21 +0200

    doing responsive with mobile first

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -181,8 +181,8 @@
 							<div class="absolute inset-0 flex items-center" aria-hidden="true">
 								<div class="w-full border-t border-gray-200" />
 							</div>
-							<div class="relative flex justify-center text-sm font-medium leading-6">
-								<span class="bg-white px-6 text-gray-900 rounded-md">Or continue with</span>
+							<div class="relative flex justify-center text-[15px] sm:text-sm font-medium leading-6">
+								<span class="bg-white px-2 sm:px-6 text-gray-900 rounded-md">Or continue with</span>
 							</div>
 						</div>
 
