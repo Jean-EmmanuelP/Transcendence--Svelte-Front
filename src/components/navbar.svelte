f494c12f9f7be9e5808d80f113521a4a7ddf6971
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-26 20:48:26 +0200

    new logic for the mobile menu better one

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -20,9 +20,10 @@
 	$: isMobile = Device.isMobile;
 
 	let user: AuthenticationType;
-	let MobileMenuOpen = false;
+	let MobileMenuOpen = true;
 	let pendingRequests: Request[] = [];
-	let isModalOpen = $modalOpen === 'userMenu' || $modalOpen === 'notifications';
+	let isModalOpen =
+		$modalOpen === 'userMenu' || $modalOpen === 'notifications' || $modalOpen === 'mobileMenu';
 	let term = '';
 	let users: searchUser[] = [];
 	let isDropdownOpen: boolean = false;
@@ -32,7 +33,8 @@
 	});
 	onDestroy(unsubscribe);
 
-	$: isModalOpen = $modalOpen === 'userMenu' || $modalOpen === 'notifications';
+	$: isModalOpen =
+		$modalOpen === 'userMenu' || $modalOpen === 'notifications' || $modalOpen === 'mobileMenu';
 	$: if (!isModalOpen && $modalOpen !== null) {
 		modalOpen.set(null);
 	}
@@ -109,7 +111,7 @@
 </script>
 
 <header
-	class="backdrop-blur-md p-1 lg:static lg:overflow-y-visible text-white border-b border-b-white/10 shadow-sm"
+	class="relative backdrop-blur-md p-1 lg:static lg:overflow-y-visible text-white border-b border-b-white/10 shadow-sm"
 >
 	<div class="flex items-center justify-center">
 		<div
@@ -227,10 +229,7 @@
 					type="button"
 					class="relative -mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
 					aria-expanded="false"
-					on:click={() => {
-						MobileMenuOpen = !MobileMenuOpen;
-						console.log('you clicked here, this is the value of the mobile menu:', MobileMenuOpen);
-					}}
+					on:click={() => toggleModal('mobileMenu')}
 				>
 					<span class="absolute -inset-0.5" />
 					<span class="sr-only">Open menu</span>
@@ -338,21 +337,6 @@
 	</div>
 
 	<!-- Mobile menu, show/hide based on menu state. -->
-	<nav class={`${MobileMenuOpen ? 'block' : 'hidden'}`} aria-label="Global">
-		<div class="flex flex-col border-y border-gray-600 pt-2 pb-4">
-			<header class="flex flex-row justify-start pl-2 pb-4">
-				<div class="flex flex-row gap-2">
-					<img class="h-6 w-6 rounded-full" src={user.avatar} alt="" />
-					<p class="text-base font-medium text-white">{user.name.split(' ', 1)}</p>
-				</div>
-			</header>
-			<div class="flex justify-around">
-				<a href={`/notifications`} class="rounded-md bg-slate-600/40 shadow-md ring-1 ring-slate-500 p-2">Notifications</a>
-				<a href={`/profile/${user.pseudo}`} class="rounded-md bg-slate-600/40 shadow-md ring-1 ring-slate-500 p-2">Profile</a>
-				<a href="/settings" class="rounded-md bg-slate-600/40 shadow-md ring-1 ring-slate-500 p-2">Settings</a>
-			</div>
-		</div>
-	</nav>
 </header>
 <ModalWrapper bind:isOpen={isModalOpen}>
 	{#if $modalOpen === 'userMenu'}
@@ -425,5 +409,72 @@
 				{/if}
 			</ul>
 		</div>
+	{:else if $modalOpen === 'mobileMenu'}
+		<nav
+			class="fixed bg-white p-2 rounded-md top-16 right-2 flex origin-top-right w-1/4"
+			aria-label="Global"
+		>
+			<div class="flex flex-col w-full pt-2 pb-4 gap-4">
+				<a
+					href={`/notifications`}
+					class="rounded-md bg-slate-600/10 shadow-md text-[15px] ring-1 ring-slate-500/20 p-2 flex items-center justify-center"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+						/>
+					</svg>
+				</a>
+				<a
+					href={`/profile/${user.pseudo}`}
+					class="rounded-md bg-slate-600/10 shadow-md text-[15px] ring-1 ring-slate-500/20 p-2 flex items-center justify-center"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+						/>
+					</svg>
+				</a>
+				<a
+					href="/settings"
+					class="rounded-md bg-slate-600/10 shadow-md text-[15px] ring-1 ring-slate-500/20 p-2 flex items-center justify-center"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+						/>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+						/>
+					</svg>
+				</a>
+			</div>
+		</nav>
 	{/if}
 </ModalWrapper>
