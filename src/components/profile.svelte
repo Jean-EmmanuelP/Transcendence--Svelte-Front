8e00573d897b53712365bb966fe91ab314d25908
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-27 14:19:24 +0200

    done for the add friend button

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -156,7 +156,7 @@
 											viewBox="0 0 24 24"
 											stroke-width="1.5"
 											stroke="currentColor"
-											class="w-6 h-6 hover:h-9 hover:w-9"
+											class="w-6 h-6"
 										>
 											<path
 												stroke-linecap="round"
