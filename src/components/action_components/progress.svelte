c9794ea1f84d0c505bcab616c99c4e510d7ecd9c
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-27 13:10:52 +0200

    doing the profilke

--- src/components/action_components/progress.svelte
+++ src/components/action_components/progress.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class={`mt-2 rounded-lg h-3 w-[60%] border bg-gray-500/10 border-gray-500/15 shadow-sm`}>
-    <div class={`rounded-l-lg h-full w-[${progress}%] bg-black`} />
+    <div class={`rounded-l-lg h-full w-[${progress}%] bg-white`} />
 </div>
--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { getUserInformationPerPseudo } from '../services/gqlUser';
-	import GameStats from '$components/statistics/game_stats.svelte';
-	import AchievmentsStats from '$components/statistics/achievments_stats.svelte';
 	import Progress from '$components/action_components/progress.svelte';
 	import type { searchUser } from '../interfaces/types';
 	import { page } from '$app/stores';
 	import { onDestroy, onMount } from 'svelte';
 	import { authentication, type AuthenticationType } from '../stores/authentication';
 	import { sendFriendRequest } from '../services/gqlFriends';
+	import OnlineUserImg from './action_components/online-user.svelte';
 
 	let isLoading = true;
 	let isError = false;
@@ -45,7 +44,7 @@
 			console.log(result);
 		} catch (error) {
 			console.error('Error sending friend request:', error.message);
-			alert(error.message)
+			alert(error.message);
 		}
 	}
 </script>
@@ -55,22 +54,36 @@
 {:else if isError}
 	<p>Somethign went wrong. Please try again.0</p>
 {:else}
-	<div class="flex flex-col h-full w-full gap-5">
-		<div class="w-full h-[40%] flex items-center flex-col mt-2">
-			<img src={user.avatar} class="rounded-full w-28 h-28 mb-2 shadow-md" alt="avatar picture" />
-			<h1 class="font-bold">{user.status}</h1>
-			<div class="flex w-full flex flex-col items-center justify-center mt-2">
-				<p class="font-medium">Level 7</p>
-				<Progress progress={30} />
-				{#if !isCurrentUserProfile}
-					<button on:click={handleSendFriendRequest} class="p-2 mt-4 rounded-md bg-red-400 text-white font-medium shadow-md hover:bg-red-500 transition duration-150 ease-linear" >Send Friend Request</button>
-				{/if}
+	<div class="flex flex-col h-full text-white w-full border border-white">
+		<div class="flex flex-row w-full h-[40%]">
+			<div class="w-1/2 h-full border border-white flex p-2 flex-row justify-around">
+				<div class="flex flex-col h-full items-center w-1/2 border border-white">
+					<img src={user.avatar} class="rounded-full h-40 w-40" alt="user picture" />
+					<p class="pt-2">{user.status === 'ONLINE' ? 'Online' : 'Offline'}</p>
+				</div>
+				<div class="border border-white flex flex-col gap-2 h-full w-1/2">
+					<div class="flex flex-row gap-2">
+						<p class="font-bold">Name:</p>
+						<h1>{user.name}</h1>
+					</div>
+					<div class="flex flex-row gap-2">
+						<p class="font-bold">Email:</p>
+						<p>{user.email}</p>
+					</div>
+					<div class="flex flex-row gap-2">
+						<p class="font-bold">Pseudo:</p>
+						<p>{user.pseudo}</p>
+					</div>
+				</div>
 			</div>
+			<div class="w-1/2 h-full border border-white" />
 		</div>
-		<div class="w-full h-[60%] flex flex-row justify-center items-center gap-5">
-			<GameStats />
-			<AchievmentsStats />
-		</div>
+		<div class="w-full h-[10%] border border-white" />
+		<div class="w-full h-[50%]" />
+		<!-- <Progress progress={30} /> -->
+		<!-- {#if !isCurrentUserProfile}
+					<button on:click={handleSendFriendRequest} class="p-2 mt-4 rounded-md bg-red-400 text-white font-medium shadow-md hover:bg-red-500 transition duration-150 ease-linear" >Send Friend Request</button>
+				{/if} -->
 	</div>
 {/if}
 
