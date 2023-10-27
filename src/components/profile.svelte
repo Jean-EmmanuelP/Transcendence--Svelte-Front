f5f9ff774f7154ab926a5ad18feabdc7dcc3a8e6
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-27 17:54:13 +0200

    done for the isCurrentUserProfile

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -61,7 +61,7 @@
 				class="flex flex-row px-2 py-2 h-1/2 w-full gap-2 bg-slate-900/20 backdrop-blur-lg rounded-md"
 			>
 				<!-- Image div to the left -->
-				<div class="relative h-full w-[21%] flex border items-center justify-start rounded-md">
+				<div class="relativ h-full w-[21%] flex items-center justify-start rounded-md">
 					<div class="relative w-full h-full">
 						<div
 							class="absolute -inset-0.5 bg-gradient-to-r from-red-800 to-blue-800 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
@@ -74,108 +74,153 @@
 					</div>
 				</div>
 				<!-- Information div to the right -->
-				<div class="pl-2 pb-2 h-full w-[35%]">
-					<div
-						class="flex gap-2 ring-1 ring-slate-800 shadow-md bg-blue-950/30 flex-col items-center justify-center mb-2 h-2/3 w-full rounded-md"
-					>
-						<h1 class="sm:font-extrabold sm:text-xl sm:tracking-wider">{user.name}</h1>
-						<p class="font-medium">Pseudo: {user.pseudo}</p>
-						<p class="font-medium">Email: {user.email}</p>
-						<div class="flex flex-row pt-2">
-							{#if user.status === 'ONLINE'}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
-									/>
-								</svg>
-								<p class="pl-2 font-medium">Offline</p>
-							{:else if user.status === 'OFFLINE'}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-									/>
-								</svg>
-								<p class="pl-2 font-medium">Online</p>
+				<div class={`pl-2 ${!isCurrentUserProfile && 'pb-2'} h-full w-[35%]`}>
+					{#if isCurrentUserProfile}
+						<div
+							class="flex gap-2 ring-1 ring-slate-800 shadow-md bg-blue-950/30 flex-col items-center justify-center mb-2 h-full w-full rounded-md"
+						>
+							<h1 class="sm:font-extrabold sm:text-xl sm:tracking-wider">{user.name}</h1>
+							<p class="font-medium">Pseudo: {user.pseudo}</p>
+							<p class="font-medium">Email: {user.email}</p>
+							<div class="flex flex-row pt-2">
+								{#if user.status === 'ONLINE'}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
+										/>
+									</svg>
+									<p class="pl-2 font-medium">Offline</p>
+								{:else if user.status === 'OFFLINE'}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+										/>
+									</svg>
+									<p class="pl-2 font-medium">Online</p>
+								{/if}
+							</div>
+						</div>
+					{:else}
+						<div
+							class="flex gap-2 ring-1 ring-slate-800 shadow-md bg-blue-950/30 flex-col items-center justify-center mb-2 h-2/3 w-full rounded-md"
+						>
+							<h1 class="sm:font-extrabold sm:text-xl sm:tracking-wider">{user.name}</h1>
+							<p class="font-medium">Pseudo: {user.pseudo}</p>
+							<p class="font-medium">Email: {user.email}</p>
+							<div class="flex flex-row pt-2">
+								{#if user.status === 'ONLINE'}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
+										/>
+									</svg>
+									<p class="pl-2 font-medium">Offline</p>
+								{:else if user.status === 'OFFLINE'}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+										/>
+									</svg>
+									<p class="pl-2 font-medium">Online</p>
+								{/if}
+							</div>
+						</div>
+						<div
+							class="flex flex-row h-1/3 w-full ring-1 ring-slate-800 shadow-md rounded-md bg-blue-950/30"
+						>
+							{#if !isCurrentUserProfile}
+								<div class="p-5 w-1/2 h-full flex items-center justify-center">
+									<div class="relative group">
+										<div
+											class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
+										/>
+										<div
+											class="relative px-5 py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md bg-slate-900 w-full h-full flex flex-row items-center justify-start"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke-width="1.5"
+												stroke="currentColor"
+												class="w-6 h-6"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M12 4.5v15m7.5-7.5h-15"
+												/>
+											</svg>
+
+											<p>Message</p>
+										</div>
+									</div>
+								</div>
+								<div class="w-1/2 h-full flex items-center justify-center">
+									<div class="relative group flex flex-row rounded-md">
+										<div
+											class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
+										/>
+										<div
+											class="relative flex flex-row bg-slate-900 px-5 py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke-width="1.5"
+												stroke="currentColor"
+												class="w-6 h-6"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+												/>
+											</svg>
+											<p class="sm:pl-2">Add friend</p>
+										</div>
+									</div>
+								</div>
 							{/if}
 						</div>
-					</div>
-					<div
-						class="flex flex-row h-1/3 w-full ring-1 ring-slate-800 shadow-md rounded-md bg-blue-950/30"
-					>
-						{#if !isCurrentUserProfile}
-							<div class="p-5 w-1/2 h-full flex items-center justify-center">
-								<div class="relative group">
-									<div
-										class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
-									/>
-									<div
-										class="relative px-5 py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md bg-slate-900 w-full h-full flex flex-row items-center justify-start"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke-width="1.5"
-											stroke="currentColor"
-											class="w-6 h-6"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="M12 4.5v15m7.5-7.5h-15"
-											/>
-										</svg>
-
-										<p>Message</p>
-									</div>
-								</div>
-							</div>
-							<div class="w-1/2 h-full flex items-center justify-center">
-								<div class="relative group flex flex-row rounded-md">
-									<div
-										class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
-									/>
-									<div
-										class="relative flex flex-row bg-slate-900 px-5 py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke-width="1.5"
-											stroke="currentColor"
-											class="w-6 h-6"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
-											/>
-										</svg>
-										<p class="sm:pl-2">Add friend</p>
-									</div>
-								</div>
-							</div>
-						{/if}
-					</div>
+					{/if}
 				</div>
 				<div class="w-[44%] h-full flex justify-center">
 					<div
