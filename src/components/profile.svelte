7f6b8eff6f2f7717d662d0de57cdd60c767fd09e
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-27 13:53:26 +0200

    added the logic online offline

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -54,11 +54,13 @@
 {:else if isError}
 	<p>Somethign went wrong. Please try again.0</p>
 {:else}
-	<div class="w-full h-full border border-white flex text-white items-center justify-center px-20 py-4">
+	<div
+		class="w-full h-full border border-white flex text-white items-center justify-center px-20 py-4"
+	>
 		<div class="border h-full w-full flex flex-col border-white">
 			<div class="flex flex-row h-1/2 w-full gap-2">
 				<div class="h-full w-[40%] flex items-center justify-center border border-red-500">
-					<img src={user.avatar} class="rounded-md h-full w-full object-contain" alt="">
+					<img src={user.avatar} class="rounded-md h-full w-full object-contain" alt="" />
 				</div>
 				<div class="h-full w-[60%] border border-red-500">
 					<div class="border border-green-500 flex items-center justify-center h-1/3 w-full">
@@ -68,10 +70,44 @@
 						<h1 class="sm:font-bold sm:text-lg">{user.pseudo}</h1>
 					</div>
 					<div class="border border-green-500 flex flex-row h-1/3 w-full">
-						<div class="w-1/3 h-full">{user.status}</div>
+						<div class="w-1/3 h-full flex items-center justify-center">
+							{#if user.status !== 'ONLINE'}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
+									/>
+								</svg>
+								<p class="pl-2">Offline</p>
+							{:else}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+									/>
+								</svg>
+								<p class="pl-2">Online</p>
+							{/if}
+						</div>
 						{#if !isCurrentUserProfile}
-						<div class="w-1/3 h-full">message</div>
-						<div class="w-1/3 h-full">A</div>
+							<div class="w-1/3 h-full flex items-center justify-center">message</div>
+							<div class="w-1/3 h-full flex items-center justify-center">A</div>
 						{/if}
 					</div>
 				</div>
@@ -83,8 +119,7 @@
 			</div>
 		</div>
 	</div>
-
-	{/if}
+{/if}
 
 <!-- <Progress progress={30} /> -->
 <!-- {#if !isCurrentUserProfile}
