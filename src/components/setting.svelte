df7e8a727eba88729f0fa7ef109e6c3152224a90
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-27 12:14:39 +0200

    now in the good position for the yes delete my account

--- src/components/setting.svelte
+++ src/components/setting.svelte
@@ -310,7 +310,7 @@
 		<div>
 			<h2 class="text-base font-semibold leading-7">Delete account</h2>
 			<p class="mt-1 text-sm leading-6 text-gray-300">
-				No longer want to use our service? You can delete your account here. This action is not
+				No longer want to play our Pong? You can delete your account here. This action is not
 				reversible. All information related to this account will be deleted permanently.
 			</p>
 		</div>
@@ -318,7 +318,7 @@
 		<form class="flex items-start md:col-span-2" on:submit|preventDefault={handleDeleteAccount}>
 			<button
 				type="submit"
-				class="rounded-md bg-red-500/20 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-red-500/30 text-white"
+				class="sm:mt-10 rounded-md bg-red-500/20 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-red-500/30 text-white"
 				>Yes, delete my account</button
 			>
 		</form>
