aec80024137130db8d43c8e6530939a674a5eb08
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-30 20:58:58 +0100

    no more problem

--- src/routes/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9/stats/+page.svelte
+++ src/routes/docs/components/7A2B9F4C1E6D3A8F5B0C7D2A1E9/stats/+page.svelte
@@ -1,10 +1,6 @@
 <script>
-    import GameStats from "$components/statistics/game_stats.svelte";
-	import AchievmentsStats from "$components/statistics/achievments_stats.svelte";
 </script>
 
 <div class="flex gap-5 flex-col">
-    <GameStats />
     <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
-    <AchievmentsStats />
 </div>
