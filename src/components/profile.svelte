6a3d091a44a33371533c486496c621a208883517
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-30 13:35:35 +0100

    arranging the recent games

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -265,6 +265,7 @@
 					<div
 						class="ring-1 ring-slate-800 shadow-md bg-blue-950/30 rounded-md bg-slate-900 w-full h-full flex flex-col"
 					>
+					<div class="w-full relative  border border-white h-[20%]">
 						<div class="relative flex flex-row items-center justify-center">
 							<div class="absolute transform -translate-x-16 translate-y-1">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
@@ -284,6 +285,10 @@
 							</div>
 							<p class="text-center pt-2 font-bold pl-2">Recent games</p>
 						</div>
+					</div>
+					<div class="w-full h-[60%] border border-red-500">
+
+					</div>
 					</div>
 				</div>
 			</div>
