fb5d68f11b922ed3d37d6f1a71bd80886d61f2d0
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-10-31 08:37:19 +0100

    doing the mobile still in the profile

--- src/components/action_components/progress.svelte
+++ src/components/action_components/progress.svelte
@@ -3,7 +3,7 @@
     export let level: number = 7;
 </script>
 
-<div class={`mt-2 flex flex-row items-center justify-center relative rounded-lg h-5 p-2 w-[60%] border bg-gray-500/10 border-gray-500/15 shadow-sm`}>
+<div class={`mt-2 flex flex-row items-center justify-center relative rounded-lg h-3 sm:h-5 w-[60%] border bg-gray-500/10 border-gray-500/15 shadow-sm`}>
     <div class={`absolute inset-0 rounded-l-lg h-full w-[${progress}%] bg-white`} />
-    <div class="absolute top-6 left-2 text-xs">Level {level}</div>
+    <div class="absolute top-6 left-2 text-[10px] sm:text-xs">Level {level}</div>
 </div>
--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -19,7 +19,6 @@
 	});
 	onDestroy(unsubscribe);
 
-	$: isMobile = Device.isMobile;
 	$: {
 		isCurrentUserProfile = userStore.pseudo === userPseudo;
 	}
@@ -61,7 +60,7 @@
 {:else if isError}
 	<p>Somethign went wrong. Please try again.0</p>
 {:else}
-	<div class="w-full h-full flex text-white items-center justify-center px-20 py-4 overflow-y-auto no-scrollbar">
+	<div class="w-full h-full flex flex-col sm:flex-row text-white items-center justify-center px-20 py-4 overflow-y-auto no-scrollbar">
 		<div class="h-full w-full flex flex-col">
 			<!-- le haut de ma page -->
 			<div
@@ -81,14 +80,14 @@
 					</div>
 				</div>
 				<!-- Information div to the right -->
-				<div class={`pl-2 ${!isCurrentUserProfile && 'pb-2'} h-full w-full sm:w-[35%]`}>
+				<div class={`pl-2 ${!isCurrentUserProfile && 'sm:pb-2'} h-full w-full sm:w-[35%]`}>
 					{#if isCurrentUserProfile}
 						<div
-							class="flex gap-2 ring-1 ring-slate-800 shadow-md bg-slate-900/40 flex-col items-center justify-center mb-2 h-full w-full rounded-md"
+							class="flex gap-1 sm:gap-2 ring-1 ring-slate-800 shadow-md bg-slate-900/40 flex-col items-center justify-center mb-2 h-full w-full rounded-md"
 						>
-							<h1 class="sm:font-extrabold sm:text-xl sm:tracking-wider">{user.name}</h1>
-							<p class="font-medium">Pseudo: {user.pseudo}</p>
-							<p class="font-medium">Email: {user.email}</p>
+							<h1 class="font-bold sm:font-extrabold text-sm sm:text-xl sm:tracking-wider">{user.name}</h1>
+							<p class="text-xs sm:text-base font-medium">Pseudo: {user.pseudo}</p>
+							<p class="text-xs sm:text-base font-medium">Email: {user.email}</p>
 							<div class="flex flex-row pt-2">
 								{#if user.status === 'ONLINE'}
 									<svg
@@ -105,7 +104,7 @@
 											d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
 										/>
 									</svg>
-									<p class="pl-2 font-medium">Offline</p>
+									<p class="pl-2 text-xs sm:text-base font-medium">Offline</p>
 								{:else if user.status === 'OFFLINE'}
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -121,7 +120,7 @@
 											d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
 										/>
 									</svg>
-									<p class="pl-2 font-medium">Online</p>
+									<p class="pl-2 text-xs sm:text-base font-medium">Online</p>
 									{/if}
 								</div>
 								<div class="w-full flex items-center justify-center">
@@ -130,12 +129,12 @@
 						</div>
 					{:else}
 						<div
-							class="flex gap-2 ring-1 ring-slate-800 shadow-md bg-slate-900/40 flex-col items-center justify-center mb-2 h-2/3 w-full rounded-md"
+							class="flex gap-1 sm:gap-2 ring-1 ring-slate-800 shadow-md bg-slate-900/40 flex-col items-center justify-center sm:mb-2 h-2/3 w-full rounded-md"
 						>
-							<h1 class="sm:font-extrabold sm:text-xl sm:tracking-wider">{user.name}</h1>
-							<p class="font-medium">Pseudo: {user.pseudo}</p>
-							<p class="font-medium">Email: {user.email}</p>
-							<div class="flex flex-row pt-2">
+							<h1 class="sm:font-extrabold font-bold text-sm sm:text-xl sm:tracking-wider">{user.name}</h1>
+							<p class="text-xs sm:text-base font-semibold sm:font-medium">Pseudo: {user.pseudo}</p>
+							<p class="text-xs sm:text-base font-semibold sm:font-medium">Email: {user.email}</p>
+							<div class="flex flex-row pt-1 sm:pt-2">
 								{#if user.status === 'ONLINE'}
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -151,7 +150,7 @@
 											d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
 										/>
 									</svg>
-									<p class="pl-2 font-medium">Offline</p>
+									<p class="pl-2 text-xs sm:text-base font-medium">Offline</p>
 								{:else if user.status === 'OFFLINE'}
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -167,21 +166,21 @@
 											d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
 										/>
 									</svg>
-									<p class="pl-2 font-medium">Online</p>
+									<p class="pl-2 text-xs sm:text-base font-medium">Online</p>
 								{/if}
 							</div>
 						</div>
 						<div
-							class="flex flex-row h-1/3 w-full ring-1 ring-slate-800 shadow-md rounded-md bg-slate-900/40"
+							class="flex flex-col sm:flex-row sm:h-1/3 w-full ring-1 ring-slate-800 shadow-md rounded-md bg-slate-900/40"
 						>
 							{#if !isCurrentUserProfile}
-								<div class="p-5 w-1/2 h-full flex items-center justify-center">
+								<div class="p-5 w-full sm:w-1/2 h-full flex items-center justify-center">
 									<div class="relative group">
 										<div
 											class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
 										/>
 										<div
-											class="relative px-5 py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md bg-slate-900/40 w-full h-full flex flex-row items-center justify-start"
+											class="relative px-2 py-1 sm:px-5 sm:py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md bg-slate-900/40 w-full h-full flex flex-row items-center justify-start"
 										>
 											<svg
 												xmlns="http://www.w3.org/2000/svg"
@@ -189,7 +188,7 @@
 												viewBox="0 0 24 24"
 												stroke-width="1.5"
 												stroke="currentColor"
-												class="w-6 h-6"
+												class="w-4 h-4 sm:w-6 sm:h-6"
 											>
 												<path
 													stroke-linecap="round"
@@ -198,17 +197,17 @@
 												/>
 											</svg>
 
-											<p>Message</p>
+											<p class="sm:text-base text-xs">Message</p>
 										</div>
 									</div>
 								</div>
-								<div class="w-1/2 h-full flex items-center justify-center">
+								<div class="w-full sm:w-1/2 h-full flex items-center pb-2 sm:pb-auto justify-center">
 									<div class="relative group flex flex-row rounded-md">
 										<div
 											class="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-lg blur opacity-50 group-hover:opacity-75 transition duration-1000 group-hover:duration-200 animate-tilt"
 										/>
 										<div
-											class="relative flex flex-row bg-slate-900/40 px-5 py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md"
+											class="relative flex flex-row bg-slate-900/40 px-2 py-1 sm:px-5 sm:py-2.5 ring-1 ring-slate-700 shadow-md hover:font-bold rounded-md"
 										>
 											<svg
 												xmlns="http://www.w3.org/2000/svg"
@@ -216,7 +215,7 @@
 												viewBox="0 0 24 24"
 												stroke-width="1.5"
 												stroke="currentColor"
-												class="w-6 h-6"
+												class="w-4 h-4 sm:w-6 sm:h-6"
 											>
 												<path
 													stroke-linecap="round"
@@ -224,7 +223,7 @@
 													d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
 												/>
 											</svg>
-											<p class="sm:pl-2">Add friend</p>
+											<p class="sm:pl-2 sm:text-base text-xs">Add friend</p>
 										</div>
 									</div>
 								</div>
@@ -232,7 +231,7 @@
 						</div>
 					{/if}
 				</div>
-				<div class="w-[44%] h-full flex justify-center">
+				<div class="w-full sm:w-[44%] h-full mt-7 sm:mt-auto flex justify-center">
 					<div
 						class="ring-1 ring-slate-800 shadow-md rounded-md bg-slate-900/40 w-full h-full flex flex-col"
 					>
@@ -271,7 +270,7 @@
 				</div>
 			</div>
 			<!-- le bas de ma page -->
-			<div class="w-full h-[40%] flex gflex-row">
+			<div class="w-full h-[40%] hidden sm:flex flex-row">
 				<div class="w-1/2 h-full flex justify-center p-2">
 					<div
 						class="ring-1 ring-slate-800 shadow-md relative bg-slate-900/40 rounded-md w-full h-full flex flex-col"
