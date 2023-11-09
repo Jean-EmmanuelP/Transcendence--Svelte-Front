d1ab25cc94b19252c7273f01b1ab5ec9f8bdc3e1
Perramant Jean-Emmanuel <perramant.work@gmail.com>
2023-11-09 22:52:34 +0100

    added the padding

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -150,7 +150,7 @@
 									autocomplete="email"
 									on:change={handleChange}
 									bind:value={$form.email}
-									class="block w-full rounded-md border-0 p-0 sm:py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600/40 sm:text-sm sm:leading-6"
+									class="block p-2 w-full rounded-md border-0 sm:py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600/40 sm:text-sm sm:leading-6"
 								/>
 							</div>
 							{#if $errors.email}
@@ -170,7 +170,7 @@
 									on:change={handleChange}
 									bind:value={$form.password}
 									autocomplete="current-password"
-									class="block w-full rounded-md border-0 p-0 sm:py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600/40 text-[14px] sm:text-sm sm:leading-6"
+									class="block p-2 w-full rounded-md border-0 sm:py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600/40 text-[14px] sm:text-sm sm:leading-6"
 								/>
 							</div>
 							{#if $errors.password}
